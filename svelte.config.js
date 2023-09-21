6bbeed069625bcd517bc67bdf72ecf4f41808711
Peter Organisciak <organisciak@gmail.com>
2023-09-21 10:50:04 -0600

    Update config based on starter

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,20 @@
+import { vitePreprocess } from '@sveltejs/kit/vite';
 import vercel from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';	
 
 //const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess({})],
 
 	kit: {
-		
-		adapter: vercel({
-
-		}),
+		adapter: vercel({}),
 		paths: {
 			base: '',
-			assets: '', // dev ? '/Users/peter.organisciak/Documents/projects/organisciak.github.io/static' : '',
+			assets: '' // dev ? '/Users/peter.organisciak/Documents/projects/organisciak.github.io/static' : '',
 		},
-		appDir: 'internal',
-	  }
+		appDir: 'internal'
+	}
 };
 
 export default config;

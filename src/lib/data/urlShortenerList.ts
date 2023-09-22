53832887aaf012bb2bc7cc2e2003664abe6730ad
Peter Organisciak <organisciak@gmail.com>
2023-09-21 23:01:44 -0600

    Add shortlinks

--- src/lib/data/urlShortenerList.ts
+++ src/lib/data/urlShortenerList.ts
@@ -9,6 +9,10 @@ export const urlMappings: UrlMapping[] = [
     "destination": "https://du.leganto.exlibrisgroup.com/leganto/public/01UODE_INST/lists/47414179690002766?auth=SAML",
   },
   {
+    "shorturl": "chat-strategies",
+    "destination": "https://github.com/organisciak/Scripting-Course/blob/master/docs/chat-model-strategies.md",
+  }
+  {
     "shorturl": "book",
     "destination": "https://outlook.office.com/bookwithme/user/c18251ee829c449d93137a48095330c2@du.edu?anonymous&ep=plink",
   }

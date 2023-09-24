9535c9e94675cd2b8f66eda26501b68551fe993a
Peter Organisciak <organisciak@gmail.com>
2023-09-23 18:10:47 -0600

    Prettier

--- src/lib/data/urlShortenerList.ts
+++ src/lib/data/urlShortenerList.ts
@@ -10,11 +10,13 @@ export const urlMappings: UrlMapping[] = [
       "https://du.leganto.exlibrisgroup.com/leganto/public/01UODE_INST/lists/47414179690002766?auth=SAML",
   },
   {
-    "shorturl": "chat-strategies",
-    "destination": "https://github.com/organisciak/Scripting-Course/blob/master/docs/chat-model-strategies.md",
+    shorturl: "chat-strategies",
+    destination:
+      "https://github.com/organisciak/Scripting-Course/blob/master/docs/chat-model-strategies.md",
   },
   {
-    "shorturl": "book",
-    "destination": "https://outlook.office.com/bookwithme/user/c18251ee829c449d93137a48095330c2@du.edu?anonymous&ep=plink",
-  }
+    shorturl: "book",
+    destination:
+      "https://outlook.office.com/bookwithme/user/c18251ee829c449d93137a48095330c2@du.edu?anonymous&ep=plink",
+  },
 ];

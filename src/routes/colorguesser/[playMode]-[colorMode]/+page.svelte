7b30d8304d358561dd5f1651161ac38baab7c646
Peter Organisciak <organisciak@gmail.com>
2023-10-09 17:12:22 -0600

    Tidying code

--- src/routes/colorguesser/[playMode]-[colorMode]/+page.svelte
+++ src/routes/colorguesser/[playMode]-[colorMode]/+page.svelte
@@ -4,84 +4,58 @@
     import { onMount } from 'svelte';
     import { cmykToRgb, rgbToHash } from '$lib/utils/colorTools';
     import type { RGBColor, CMYKColor } from '$lib/utils/colorTools';
-    import type { GuessHistory } from '$lib/colorguesser/types.ts';
+    import type { Guess, GuessHistory } from '$lib/colorguesser/types.ts';
     import { guessHistoryStore } from '$lib/colorguesser/guessHistoryStore';
 	import ColorBoxBase from '$lib/colorbox/ColorBoxBase.svelte';
 
-    type PlayMode = "INFINITE" | "DAILY" | "PRACTICE" | "FINISHED";
+    type PlayMode = "INFINITE" | "DAILY" | "PRACTICE";
     type ColorMode = "RGB" | "CMYK";
 
+    // init vars
     let playMode: PlayMode;
     let colorMode: ColorMode;
-    
-    // routing in +page.server.ts should ensure that these are valid
-    let { rawPlayMode, rawColorMode } = $page.params;
-    
-    $: {
-        rawPlayMode = $page.params.playMode.toUpperCase();
-        rawColorMode = $page.params.colorMode.toUpperCase();
-        playMode = rawPlayMode.toUpperCase() as PlayMode;
-        colorMode = rawColorMode.toUpperCase() as ColorMode;
-    };
-
-    
+    let guessHistory: GuessHistory = [];
+    const maxAttempts = 5;
+    let attempts: number = 0;
+    let dayScore: number = 0;
+    let lastScore: number = 0;
+    let submitted: boolean = false;
+    let finished: boolean = false;
+    let filteredGuesses: GuessHistory = [];
     let rgbColors: RGBColor = {
         red: 0,
         green: 0,
         blue: 0
-    };
-
+        };
     let cmykColors: CMYKColor = {
         cyan: 0,
         magenta: 0,
         yellow: 0,
         black: 0
-    };
-
+        };
+    function getRandomInt(max: number) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
     let target: RGBColor = {
         red: getRandomInt(255),
         green: getRandomInt(255),
         blue: getRandomInt(255)
-    };
-
-    let submitted: boolean = false;
-    let difference: number;
-    let diffRelative: number;
-    let scores: { 'INFINITE': number[], "DAILY": number[] } = { "INFINITE": [], "DAILY": [] };
-    let guessHistory: GuessHistory = [];
-
-    function getRandomInt(max: number) {
-        return Math.floor(Math.random() * Math.floor(max));
-    }
-    let attempts: number = 0;
-    let dayScore: number = 0;
+        };
     
-    $: $guessHistoryStore && updateValues(); // Recompute when guessHistoryStore changes
-
-    onMount(() => {
-        getNextColor();
-        updateValues();
-    });
-
-    function updateValues() {
-        const filteredGuesses = $guessHistoryStore.filter(guess => guess.colorMode === colorMode && guess.playMode === playMode);
-        attempts = filteredGuesses.length;
-        dayScore = filteredGuesses.reduce((acc, curr) => acc + curr.difference, 0);
-    }
+    // routing in +page.server.ts should ensure that these are valid
+    let { rawPlayMode, rawColorMode } = $page.params;
 
     function calculateDifference(guess: RGBColor, target: RGBColor) {
         /* Calculate based on RGB - cmyk should have already been converted to rgb with reactive statement */
-        difference = Math.abs(guess.red - target.red) + Math.abs(guess.green - target.green) + Math.abs(guess.blue - target.blue);
+        const difference = Math.abs(guess.red - target.red) + Math.abs(guess.green - target.green) + Math.abs(guess.blue - target.blue);
         /* Use floor to be nicer to the user */
-        diffRelative = Math.floor(100 * difference / (255*3));
+        const diffRelative = Math.floor(100 * difference / (255*3));
         return diffRelative;
     }
 
     function submitGuess() {
-        diffRelative = calculateDifference(rgbColors, target);
-        if (playMode == 'INFINITE' || playMode == 'DAILY') {
-            scores[playMode].push(diffRelative);
-        }
+        const diffRelative = calculateDifference(rgbColors, target);
+
         // Add a guess to the history
         const newGuess = {
             colorMode,
@@ -91,17 +65,13 @@
             difference: diffRelative
         };
         guessHistoryStore.update(guessHistory => [...guessHistory, newGuess]);
-        //dayScore += diffRelative; // Maximum difference score is 765 (255 * 3), so we subtract to get the score
         submitted = true;
 
-        if (playMode == "DAILY") {
-            if (attempts >= 5 && playMode !== "INFINITE") {
-                playMode = "FINISHED";
-            } else {
-                getNextColor();
-            }
+        console.log('attempts', attempts);
+        finished = (playMode == 'DAILY') && (attempts > maxAttempts);
+        if (!finished) {
+            getNextColor();
         }
-        
     }
 
     function getNextColor() {
@@ -113,6 +83,36 @@
         rgbColors = { red: 127, green: 127, blue: 127} as RGBColor;
         cmykColors = { cyan: 50, magenta: 50, yellow: 50, black: 50} as CMYKColor;
     }
+
+    function guessFilter(guess: Guess, colorMode: ColorMode, playMode: PlayMode): boolean {
+        return guess.colorMode === colorMode && guess.playMode === playMode;
+    }
+
+    function updateValues() {
+        filteredGuesses = $guessHistoryStore.filter(guess => guessFilter(guess, colorMode, playMode));
+        attempts = filteredGuesses.length;
+        dayScore = filteredGuesses.reduce((acc, curr) => acc + curr.difference, 0);
+        lastScore = filteredGuesses[filteredGuesses.length - 1]?.difference;
+        finished = (playMode == 'DAILY') && (attempts >= maxAttempts);
+    }
+
+    onMount(() => {
+        getNextColor();
+        updateValues();
+    });
+
+        
+    $: {
+        rawPlayMode = $page.params.playMode.toUpperCase();
+        rawColorMode = $page.params.colorMode.toUpperCase();
+        // Typed
+        playMode = rawPlayMode.toUpperCase() as PlayMode;
+        colorMode = rawColorMode.toUpperCase() as ColorMode;
+
+        updateValues();
+    };
+    
+    $: $guessHistoryStore && updateValues(); // Recompute when guessHistoryStore changes
 
     $: if (colorMode == "CMYK") {
         rgbColors = cmykToRgb(cmykColors);
@@ -140,7 +140,7 @@
     <p class="mb-4">Mode: {playMode} | {colorMode}</p>
     {/key}
     <!-- Target Color -->
-    {#if playMode == 'FINISHED'}
+    {#if finished }
         {#each guessHistory.filter(guess => guess.playMode === playMode && guess.colorMode === colorMode) as {guessColor, targetColor, difference}, index}
             <div class="flex space-x-4 items-center mb-2">
                 <!-- Display the guessed color -->
@@ -155,11 +155,11 @@
         {/each}
     {/if}
     
-    {#if playMode != 'FINISHED'}
+    {#if !finished }
         <ColorBoxBase showHex={false} csshex={rgbToHash(target)} />
     {/if}
     <!-- RGB Sliders -->
-    {#if colorMode === 'RGB' && playMode !== 'FINISHED'}
+    {#if colorMode === 'RGB' && !finished}
         {#each Object.entries(rgbColors) as [color, value], index (color)}
             <div class="mb-2">
                 <label class="block text-center">
@@ -172,10 +172,9 @@
     {/if}
 
 
-    {#if colorMode === 'CMYK' && playMode !== 'FINISHED'}
+    {#if colorMode === 'CMYK' && !finished }
         {#each Object.entries(cmykColors) as [color, value], index (color)}
             <div class="mb-2">
-                {color}, {value}
                 <label class="block text-center">
                     {color[0].toUpperCase() + color.slice(1)}
                     <input type="range" min="0" max="100" value={value} on:input={(e) => cmykColors[color] = +e.target.value ?? 0} class="w-48" />
@@ -186,12 +185,15 @@
     {/if}
 
     <!-- Submit & Score Display -->
-    {#if playMode !== 'PRACTICE' && playMode !== 'FINISHED'}
-        <p class="mb-2">Attempts: {attempts}</p>
+    {#if playMode !== 'PRACTICE' && !finished }
         <button class="px-4 py-2 bg-blue-500 text-white rounded" on:click={submitGuess}>Submit</button>
-        {#if submitted}
-            <p class="mt-2">Difference: {difference} ({diffRelative})</p>
+        
+        {#if playMode === 'DAILY'}
+        <p class="mb-2">Attempts: {attempts}</p>
+        <p class="mt-2">Score: {dayScore}{#if submitted}(Last guess: {lastScore}){/if}</p>
         {/if}
-        <p class="mt-2">Day Score: {dayScore}</p>
+        {#if playMode === 'INFINITE'}
+        <p class="mt-2">Avg Score: {Math.floor(dayScore/attempts)}{#if submitted}(Last guess: {lastScore}){/if}</p>
+        {/if}
     {/if}
 </div>

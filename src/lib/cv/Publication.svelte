fbc74f503a1e20f01aba5cbd6da389fad85a2cc9
Peter Organisciak <organisciak@gmail.com>
2023-11-18 09:58:07 -0700

    CV updates

--- src/lib/cv/Publication.svelte
+++ src/lib/cv/Publication.svelte
@@ -2,19 +2,103 @@
   import type { CSLPublication } from './types';
   export let data: CSLPublication;
 
-  function truncate_url(url: string, max_length: number = 30) {
-    if (url.length > 50) {
+  let copied = false;
+
+  function truncate_url(url: string, max_length: number = 50) {
+    if (url.length > max_length) {
       const parts: string[] = url.split('/');
       return parts.slice(0,3).join('/')
     }
     return url
   }
 
+  function urlToDOIShort(url: string): string {
+    if (url.startsWith('http')) {
+        url = url.split('doi.org/')[1];
+    }
+    return url;
+}
+  function doiForceLong(doi: string): string {
+    if (!doi.startsWith('http')) {
+        doi = 'https://doi.org/' + doi;
+    }
+    return doi;
+  }
+
+  function doiForceShort(doi: string): string {
+    if (doi.startsWith('http')) {
+        doi = doi.split('doi.org/')[1];
+    }
+    return doi;
+  }
+
+  // written by ChatGPT: https://chat.openai.com/c/0fa52fa7-f4b4-4ad4-b317-39ca82a4b048
+  function formatAPA(publication: CSLPublication, asHTML:boolean = false): string {
+    // Format authors
+    let authors = publication.author.map(a => `${a.family}, ${a.given.charAt(0)}.`).join(', ');
+
+    // Format year
+    let year = publication.issued['date-parts'][0][0];
+
+    // Format title
+    let title = publication.title;
+
+    // Format journal name, volume, and issue
+    let journal = publication['container-title'] ? `${publication['container-title']}` : '';
+    let volume = publication.volume ? `${publication.volume}` : '';
+    let issue = publication.issue ? `(${publication.issue})` : '';
+    if (volume !== '' || issue !== '') journal += ', ';
+
+    if (asHTML) {
+      journal = `<i>${journal}</i>`;
+    }
+
+    // Format page numbers
+    let pages = publication.page ? `, ${publication.page}` : '';
+
+    // Format DOI or URL
+    let doiOrUrl = ''
+    if (publication.DOI && publication.DOI.startsWith('http')) {
+      doiOrUrl = publication.DOI;
+    } else if (publication.DOI) {
+      doiOrUrl = `doi:${publication.DOI}`;
+    } else if (publication.URL) {
+      doiOrUrl = `Retrieved from ${publication.URL}`;
+    }
+    
+
+    // Combine all parts
+    return `${authors} (${year}). ${title}. ${journal}${volume}${issue}${pages}. ${doiOrUrl}`;
+  }
+
+
+  async function copyToClipboard(data: CSLPublication) {
+    console.log(data);
+    const txt = formatAPA(data, false);
+    const html = formatAPA(data, true);
+    console.log(txt, html);
+    try {
+      await navigator.clipboard.write([
+        new ClipboardItem({
+          'text/html': new Blob([html], { type: 'text/html' }),
+          'text/plain': new Blob([txt], { type: 'text/plain' })
+        })
+      ]);
+      copied = true;
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
+  }
+
+
+
+  let showAbstract = false;
+
 </script>
 <!---
 TODO allow customizing the itemtype
 -->
-<div class="text-sm publication flex-auto w-96 dark:bg-slate-700 bg-slate-200 m-2 rounded-lg p-5" itemscope itemtype="http://schema.org/ScholarlyArticle">
+<div class="text-sm publication flex-auto w-80  dark:bg-slate-700 bg-slate-200 m-2 rounded-lg p-5" itemscope itemtype="http://schema.org/ScholarlyArticle">
   <h2 class="flex text-lg" itemprop="headline">{data.title}</h2>
   
   {#if data.event}
@@ -59,7 +143,12 @@ TODO allow customizing the itemtype
     </p>
   {/if}
 
-  {#if data.URL}
+  {#if data.DOI}
+  <p>
+    <strong>DOI:</strong> <a href={doiForceLong(data.DOI)} target="_blank" rel="noopener noreferrer" itemprop="url">{doiForceShort(data.DOI)}</a>
+  </p>
+  {/if}
+  {#if data.URL && urlToDOIShort(data.URL) != data.DOI}
     <p>
       <strong>URL:</strong> <a href={data.URL} target="_blank" rel="noopener noreferrer" itemprop="url">{truncate_url(data.URL)}</a>
     </p>
@@ -79,5 +168,22 @@ TODO allow customizing the itemtype
         <br/>
       {/each}
     </p>
+  {/if}
+  {#if data.abstract}
+    <button on:click={() => showAbstract = !showAbstract}
+      class='dark:bg-slate-600 hover:dark:bg-slate-500 inline-block bg-slate-300 hover:bg-slate-400 leading-tight font-bold text-slate-200 p-1 rounded-lg text-xs my-1'>
+      ↓ Abstract
+    </button>
+  {/if}
+
+  
+  <button on:click={() => copyToClipboard(data)} class='dark:bg-slate-600 hover:dark:bg-slate-500 inline-block bg-slate-300 hover:bg-slate-400 leading-tight font-bold text-slate-200 p-1 rounded-lg text-xs my-1'>Copy APA</button>
+  {#if copied}<p class='text-xs italic inline-block text-slate-500'>Copied</p>{/if}
+  
+
+  {#if data.abstract && showAbstract}
+    <div class="text-xs rounded-lg dark:bg-slate-600 bg-slate-300 p-2 mt-1">
+      {data.abstract}
+    </div>
   {/if}
 </div>
--- src/routes/cv/+page.svelte
+++ src/routes/cv/+page.svelte
@@ -60,109 +60,27 @@ peer-reviewed/refereed. (sub: April 2, 2018, start: July 2018, end: June
   `
 
 	const source = `
-## Panels
-
-
-Acar, S., Lee, L. E., Rinn-McCann, A., Mun, R., Rambo-Hernandez, K., Berthiaume, K., Dumas, D.,
-**Organisciak, P.**, Flemister, T., Grajzel, K., Newman, M., Eby, D., Deshayes, C., Puryear, J. (2023).
-"Classic and Novel Approaches in Creativity Assessment and Programming: Implications for Gifted Education,"
-Annual Meeting, AERA, Chicago, IL.
-
-
-Boughida, K., Zhang, B., **Organisciak, P.**, D’Agostino, S., (2023)
-"ChatGPT: The Future of Higher Ed and Libraries, Brought to You by AI,"
-CNI Spring 2023 Membership Meeting,  Coalition for Networked Information, Denver, CO, USA.
-
-
-Booth, K., Sanchez, J., Bishop, B. W., **Organisciak, P.** (2023).
-"Resilience in iSchools cohort education during a turbulent time: Improved normalcy through systemic inclusivity and other lessons learned to optimize virtual and physical spaces,"
-iConference 2023, iSchools Consortium, Barcelona, Spain.
-
-
-Bishop, B. W., **Organisciak, P.**, Mayernik M., Cowan, M. (2021).
-\"Assessing the Research Data Management Landscape Through Practice and
-Education,\" Federation of Earth Science Information Partners, Online.
-
-
-Ruecker, S., Rockwell, G., **Organisciak, P.**, Meunier, J.-G.,
-Eberle-Sinatra, M., Galey, A., Morrison, A., Siemens, R. (2009). Annual
-Meeting of the Society for Digital Humanities, \"Graduate Education in
-the Digital Humanities,\" Carleton University, Ottawa, Canada.
-
-
-## Posters
-
-
-*Conference posters associated with a peer-reviewed short abstract.*
-
-
-Durward, M., **Organisciak, P.**, Matusiak, K. (2020). \"Advancing
-Technologies for Traditional Library Services: Utilizing Content Based
-Recommendation to Assist in Readers\' Advisory,\" World Library and
-Information Congress (WLIC), International Federation of Library
-Associations and Institutions, Dublin, Ireland.
-
-
-Ryan, M., Gypin, L., Matusiak, K., Schmidt, B. M., **Organisciak, P.**,
-(2020). \"Conceptual Modeling of Similarities and Duplication in
-Large-Scale Digital Libraries,\" Digital Humanities 2020, Alliance for
-Digital Humanities Organization, Ottawa.
-
-
-**Organisciak, P.**, Pares, N. (2020). \"MOODs: Identifying Outlier
-Passages and Texts in the Legislative Process,\" 6th International
-Conference on Computational Social Science. Cambridge, MA/Online.
-
-
-**Organisciak, P.** (2018). \"Including Phrases in Bags of Words,\"
-Uncamp 2018, HathiTrust Research Center, Berkeley.
-
-
-**Organisciak, P.**, Teevan, J., Twidale, M. B. (2018). \"Asking the
-Right Way: Design Interventions for Microtasks,\" The sixth AAAI
-Conference on Human Computation and Crowdsourcing, Association for
-Advancement of Artificial Intelligence, Quebec City, Canada.
-
-
-**Organisciak, P.**, Teevan, J., Dumais, S., Miller, R. C., Kalai, A.
-(2013), \"Personalized Human Computation,\" Conference on Human
-Computation and Crowdsourcing (HCOMP 2013). AAAI, Palm Springs, CA.
-
-
-**Organisciak, P.**, Rockwell, G., Sinclair, S., Ruecker, S., (2009).
-\"Text Analysis for me Too: An embeddable text analysis widget,\" Annual
-Meeting of the Society for Digital Humanities, SDH/SEMI.
-
-
-Sinclair, S., Beacon, D., Brown, S., Downie, J. S., Fiorentino, C.,
-Juola, P., **Organisciak, P.**, Rockwell, G., Schreibman, S., Uszkalo,
-K. (2009). \"TADA Research Evaluation Exchange: Winning 2008
-Submissions,\" Digital Humanities 2009, University of Maryland, USA.
-
 
 # Products
 
 
 ## Research Websites and Tools
 
-
-Open Creativity Scoring (OCS) -- <https://openscoring.du.edu>\
-(2207 unique users, June 2020-August 2022)
-SaDDL -- [https://saddl.du.edu](https://saddl.du.edu/)
-Bookworm -- <https://bookworm.htrc.illinois.edu>\
-Bookworm Playground - <https://bookworm.htrc.illinois.edu/app/>
+- Open Creativity Scoring (OCS) -- <https://openscoring.du.edu>
+  - (2207 unique users, June 2020-August 2022)
+- SaDDL -- [https://saddl.du.edu](https://saddl.du.edu/)
+- Bookworm -- <https://bookworm.htrc.illinois.edu>
+- Bookworm Playground - <https://bookworm.htrc.illinois.edu/app/>
 
 
 ## Software
 
-
-HTRC Features Reader - <http://github.com/htrc/htrc-feature-reader>\
-A mature, long-developed library for working with non-consumptive book
+- HTRC Features Reader - <http://github.com/htrc/htrc-feature-reader>\
+  - A mature, long-developed library for working with non-consumptive book
 data.
 
-
-Open Scoring - <https://github.com/massivetexts/open-scoring>\
-Tools for creativity scoring.
+- Open Scoring - <https://github.com/massivetexts/open-scoring>\
+  - Tools for creativity scoring.
 
 
 # Teaching
@@ -377,6 +295,10 @@ Human-Centered Data Science and Social Computing. (October 2, 2017).
 
 *Since 2017*.
 
+- Creativity Research Journal (2023-)
+
+- Behavioral Research Methods (2022-)
+
 - Journal of the Association for Information Science & Technology (2022).
 
 - Advances in Cognitive Psychology (2022).
@@ -403,259 +325,6 @@ Human-Centered Data Science and Social Computing. (October 2, 2017).
 - Telematics and Informatics. (June 2019).
 
 - Transactions on Information Systems. (October 2017).
-
-# Consulting
-
-Higher Education, University of North Texas, Denton, TX. (May 2022 -
-July 2022).
-
-Working with Dr. Selcuk Acar on developing a pilot of methods for
-scoring originality and elaboration in figural (i.e., drawn) tests of
-creativity.
-
-Higher Education, Université du Québec à Montréal, Montreal, Canada.
-(August 2021).
-
-Working with Dr. Pier-Luc de Chantel to develop an API to Open
-Creativity Scoring, making that system's scoring methods available to
-use in other tools and websites, such as Qualtrics.
-
-# Products
-
-## Research Websites and Tools
-
-- Open Creativity Scoring (OCS) -- <https://openscoring.du.edu>\
-(2207 unique users, June 2020-August 2022)
-- SaDDL -- [https://saddl.du.edu](https://saddl.du.edu/)
-- Bookworm -- <https://bookworm.htrc.illinois.edu>\
-- Bookworm Playground - <https://bookworm.htrc.illinois.edu/app/>
-
-## Software
-
-HTRC Features Reader - <http://github.com/htrc/htrc-feature-reader>\
-A mature, long-developed library for working with non-consumptive book
-data.
-
-Open Scoring - <https://github.com/massivetexts/open-scoring>\
-Tools for creativity scoring.
-
-# Teaching
-
-## Teaching Experience
-
-### Courses
-
-LIS 4015 -- User and Access Services
-
-3 credit hours (in-person). 2 credit hours (online).
-
-LIS 4220 -- Data Curation
-
-3 credit hours.
-
-LIS 4235 -- Scripting for Large Databases
-
-4 credit hours. Course materials available at
-<https://github.com/organisciak/Scripting-Course>.
-
-LIS 4700DH -- Digital Humanities
-
-3 credit hours.
-
-590TXT, LIS4991TXT -- Text Mining
-
-Course materials available at
-<https://github.com/organisciak/Text-Mining-Course> (Alt metrics: 98
-stars, 29 forks). Originally taught at University of Illinois, this
-course is only taught at DU as an independent study, LIS 4991.
-
-## Directed Student Learning
-
-### Dissertation, Committee Member
-
--   Matthew Durward . \"Audience Specific Text Recommendation for
-    English Language Learners" University of Canterbury. (2021-)
-
--   Jill Talley. \"Paving pathways for success: The role of transition
-    models and disability services in postsecondary education for
-    students with disabilities\" (2021)
-
--   Paige Alfonso. \"Exploring the relations between identity and
-    digital media activity processes: A mixed-methods psychological
-    investigation into connective action\" (2019-2020)
-
-### Capstone, Committee Chair
-
--   Michele Newman. \"A Taxonomy of Game Mechanics\" (2022)
-
--   Sarah Beals. \"Crowdsourced Data and Visualizing Theme Park Wait
-    Times\" (2020)
-
--   Matthew Durward. \"Constructing a Platform to Assist English
-    Language Learners with Film Selection\" (2020)
-
--   Kevin Stevens. \"Historiography as a Signature Pedagogical Approach
-    to Information Literacy Instruction\" (2020)
-
--   Rita Zhang. \"A Framework of Active Exploratory Book
-    Recommendation\" (2020)
-
--   Lee Howick. \"Faculty Opinions and Use of Open Access Publishing at
-    the University of Denver\" (2020)
-
--   Nicolas Pares. The Effects of Research Data Management Services:
-    Associating the Data Curation Lifecycle with Open Research Output\"
-    (2020)
-
--   Vince Garin. \"How Digital Humanities Challenges Publishing and
-    Academic Labor Paradigms\" (2019)
-
--   Micah Saxton. \"Best Practices for Topic Modeling,\" Library &
-    Information Science. (2018)
-
--   Sheena Young. "Library Thunderdome: Fake News vs. Information
-    Literacy" (2018)
-
-### Capstone, Committee Member
-
--   Summer Shetenhelm. \"Copyright Statements in Plains to Peaks
-    Collective Digital Collections\" (2019)
-
--   Fatih Gunaydin. \"Assessment of Current Research Data Services in
-    Terms of Technical Services\" (2019)
-
-### Thesis, Committee Chair
-
--   Nicolas Pares. \"Human-Centered Search Process: A Grounded Theory
-    Study on the Modern Search Experience\" (2022)
-
-## Non-Credit Instruction Taught
-
-### Guest Lecture
-
--   Topic: "Introduction to Docker for Data Curation\". Data Curation,
-    University of Illinois at Urbana-Champaign. Allen Renear. (Recorded
-    for online course, 2018).
-
--   Topic: "Usability Design". Digital Libraries, University of Illinois
-    at Urbana-Champaign, J. Stephen Downie.
-
-### Guest Lecture, DU
-
--   Topic: "Digital Humanities", Organization of Information, University
-    of Denver. Krystyna Matusiak (2017-18, recorded for online course
-    2019-)
-
--   Topic: "Scholarly and Professional Communication", Foundations of
-    Library and Information Science, University of Denver. Mary
-    Stansbury (2017)
-
-## Faculty Development Activities Attended
-
-OTL Faculty Workshop or Training, \"Teaching@DU Short Course,\" Office
-of Teaching and Learning, Denver, CO, NF. (August 1, 2017 - December 31,
-2017).
-
-# Professional Service
-
-## University Service
-
-Committee Member, Open Access Fund. (June 2021 - Present).
-
-## Professional Memberships
-
-Association for the Advancement of Artificial Intelligence. (November
-2013 - Present).
-
-American Society for Information Science and Technology. (2011 -
-Present).
-
-Association for Computing Machinery. (2011 - Present).
-
-Alliance of Digital Humanities Organizations. (2010 - Present).
-
-Canadian Society for Digital Humanities. (2009 - 2013).
-
-Canadian Association for Information Science. (2009 - 2011).
-
-## Committee Chair
-
-Committee Chair, iSchools Conference Student Symposium Chair, Barcelona.
-(July 1, 2022 - March 2023).
-
-## Committee Member
-
-Committee Member, Program Committee of Digital Humanities Workshop (DHW)
-2021. (2021).
-
-Committee Member, JCDL 2019 Outreach Committee. (May 2018 - June 2019).
-
-Associate Chair, iConference 2019 Program Committee, College Park,
-Maryland. (June 2018 - February 2019).
-
-Committee Member, HCOMP Program Committee. (January 2018 - October
-2018).
-
-Committee Member, JCDL Program Committee. (January 2018 - July 2018).
-
-### Guest Editor
-
-Guest Co-Editor, Special Issue on "Conceptual Models of the
-Sociotechnical", Journal of Association for Information Science and
-Technology (JASIST) (2021-2022).
-
-### Editorial Review Board Member
-
-Editorial Review Board Member, Przegląd Biblioteczny, Warsaw. (August
-2021 - Present).
-
-Editorial Review Board Member, Journal of Open Humanities Data. (July
-2015 - Present).
-
-## Officer
-
-President, Special Interest Group on Digital Libraries. (October 2020 -
-October 2021).
-
-## Conference Organizer
-
-Organizer, First Digital Front Range Symposium, Digital Scholarship in
-the Front Range, Denver, Colorado. (May 5, 2018).
-
-## Reviewer, Grant Panelist
-
-Panelist, National Endowment for the Humanities, Washington, D.C.
-(August 2018).
-
-## Reviewer, Conference Paper
-
-*Since 2017.*
-
-Association for Information Science and Technology. (2010 - 2019),
-Digital Humanities (2009 - 2021),
-WebConf (2018-23),
-iConference (2017),
-Human-Centered Data Science and Social Computing (2017).
-
-## Reviewer, Journal Article
-
-*Since 2017*.
-
-Behavior Research Methods (2023),
-Social Network Analysis and Mining (2022-23),
-Journal of the Association for Information Science & Technology (2022),
-Advances in Cognitive Psychology (2022), 
-Digital Humanities Quarterly (2022),
-International Journal for Digital Libraries. (2022),
-Data in Brief (2022),
-Journal of Open Humanities Data (2022),
-Symmetry, (2022),
-Electronics, (2022),
-Mathematics, (2021, 2022),
-Applied Sciences (2021, 2022),
-Entropy (2021),
-Telematics and Informatics (2019),
-Transactions on Information Systems.(2017).
 
 # Consulting
 
@@ -811,12 +480,12 @@ type PresentationExtension = {
   const pubSections: {heading:string, entries:CSLPublication[], comment?:string}[] =
         [
           {
-            heading: "Book Chapters",
-            entries: typedPublications.filter(p => p.type === 'chapter')
-          },
-          {
             heading: "Journal Articles",
             entries: typedPublications.filter(p => p.type === 'article-journal')
+          },
+          {
+            heading: "Book Chapters",
+            entries: typedPublications.filter(p => p.type === 'chapter')
           },
           {
             heading: "Proceedings Paper",
@@ -875,6 +544,23 @@ type PresentationExtension = {
                       (pub.genre === 'Invited Teaching Workshop')
               )
             }) as (CSLPublication & {"genre": 'Invited Teaching Workshop'})[]
+          },
+          {
+            heading: "Panels",
+            entries: typedPublications.filter(function (pub) {
+              return (pub.type === 'speech' && 
+                      (pub.genre === 'Panel')
+              )
+            }) as (CSLPublication & {"genre": 'Panel'})[]
+          },
+          {
+            heading: "Posters",
+            comment: "Conference posters associated with a peer-reviewed short abstract.",
+            entries: typedPublications.filter(function (pub) {
+              return (pub.type === 'paper-conference' && 
+                      (pub.extra === 'Poster')
+              )
+            }) as (CSLPublication & {"genre": 'Poster'})[]
           }
   ]
 

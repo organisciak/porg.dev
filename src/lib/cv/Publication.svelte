f3b47af377065d1bd7b423f39cf7f28509a26b88
Peter Organisciak <organisciak@gmail.com>
2024-09-11 12:08:03 -0600

    Fix publication parsing when there are data errors

--- src/lib/cv/Publication.svelte
+++ src/lib/cv/Publication.svelte
@@ -8,6 +8,7 @@
   let copied = false;
 
   function truncate_url(url: string, max_length: number = 50) {
+    if (!url) return '';
     if (url.length > max_length) {
       const parts: string[] = url.split('/');
       return parts.slice(0,3).join('/')
@@ -16,12 +17,16 @@
   }
 
   function urlToDOIShort(url: string): string {
+    if (!url) return '';
     if (url.startsWith('http')) {
-        url = url.split('doi.org/')[1];
+        const parts = url.split('doi.org/');
+        return parts.length > 1 ? parts[1] : url;
     }
     return url;
-}
+  }
+
   function doiForceLong(doi: string): string {
+    if (!doi) return '';
     if (!doi.startsWith('http')) {
         doi = 'https://doi.org/' + doi;
     }
@@ -29,21 +34,23 @@
   }
 
   function doiForceShort(doi: string): string {
+    if (!doi) return '';
     if (doi.startsWith('http')) {
-        doi = doi.split('doi.org/')[1];
+        const parts = doi.split('doi.org/');
+        return parts.length > 1 ? parts[1] : doi;
     }
     return doi;
   }
 
   const customFieldsAsButtons: string[] = ['preprint', 'SSRN'];
 
-  // written by ChatGPT: https://chat.openai.com/c/0fa52fa7-f4b4-4ad4-b317-39ca82a4b048
   function formatAPA(publication: CSLPublication, asHTML:boolean = false): string {
+    if (!publication) return '';
     // Format authors
-    let authors = publication.author.map(a => `${a.family}, ${a.given.charAt(0)}.`).join(', ');
+    let authors = publication.author?.map(a => `${a.family}, ${a.given.charAt(0)}.`).join(', ');
 
     // Format year
-    let year = publication.issued['date-parts'][0][0];
+    let year = publication.issued?.['date-parts']?.[0]?.[0];
 
     // Format title
     let title = publication.title;
@@ -70,7 +77,7 @@
     let pages = publication.page ? `, ${publication.page}` : '';
 
     // Format DOI or URL
-    let doiOrUrl = ''
+    let doiOrUrl: string = '';
     if (publication.DOI && publication.DOI.startsWith('http')) {
       doiOrUrl = publication.DOI;
     } else if (publication.DOI) {
@@ -85,7 +92,7 @@
       volume = '';
       issue = '';
       pages = '';
-      doiOrUrl = publication.URL;
+      doiOrUrl = publication.URL || '';
     }
 
     // Special case for SSRN
@@ -94,7 +101,7 @@
       volume = '';
       issue = '';
       pages = '';
-      doiOrUrl = publication.URL;
+      doiOrUrl = publication.URL || '';
     }
 
     // Note for CSL with a status field of 'Under Review'
@@ -113,6 +120,7 @@
 
 
   async function copyToClipboard(data: CSLPublication) {
+    if (!data) return;
     const txt = formatAPA(data, false);
     const html = formatAPA(data, true);
     try {
@@ -140,19 +148,19 @@ TODO allow customizing the itemtype
   itemtype="http://schema.org/ScholarlyArticle">
   <div class="flex items-start">
   <h2 class="text-lg" itemprop="headline">
-      {data.title}
+      {data?.title || 'Untitled'}
   </h2>
   <span class='text-sm ml-1 dark:text-slate-500 text-slate-400' itemprop="datePublished" >
-    {data.issued['date-parts'][0][0]}
+    {data?.issued?.['date-parts']?.[0]?.[0] || 'Unknown Date'}
   </span>
   </div>
   
-  {#if data.event}
+  {#if data?.event}
     <p class='italic text-sm text-slate-600 dark:text-slate-100' itemprop="locationCreated">
       {data.event}{#if data['publisher']}, {data['publisher']}{/if}{#if data['event-place']}, {data['event-place']}{/if}
     </p>
   {/if}
-  {#if data['container-title']}
+  {#if data?.['container-title']}
   <div class="italic text-sm text-slate-600 dark:text-slate-100" itemprop="isPartOf" itemscope itemtype="https://schema.org/PublicationIssue">
       <span itemscope itemtype="https://schema.org/Periodical" itemid="#periodical">
         <span itemprop="name">{data['container-title']}</span>
@@ -160,14 +168,18 @@ TODO allow customizing the itemtype
   </div>
   {/if}
 
+  {#if data?.author}
   <p itemprop="author" itemscope itemtype="http://schema.org/Person">
     <strong>Authors:</strong>
     {#each data.author as author}
-    <span itemprop="name" class="{author.family == 'Organisciak' ? 'italic' : ''}">{author.given} {author.family}{@html author === data.author[data.author.length - 1] ? '.' : ', '}</span>
+    <span itemprop="name" class="{author.family == 'Organisciak' ? 'italic' : ''}">
+      {author.given} {author.family}{@html author === data.author[data.author.length - 1] ? '.' : ', '}
+    </span>
     {/each}
   </p>
+  {/if}
 
-  {#if data.editor}
+  {#if data?.editor}
     <p itemprop="editor" itemscope itemtype="http://schema.org/Person">
       <strong>Editors:</strong>
       {#each data.editor as editor}
@@ -176,41 +188,41 @@ TODO allow customizing the itemtype
     </p>
   {/if}
 
-  {#if data.volume}
+  {#if data?.volume}
     <p>
       <strong>Volume:</strong> <span itemprop="volumeNumber">{data.volume}</span>
     </p>
   {/if}
 
-  {#if data.edition}
+  {#if data?.edition}
     <p>
       <strong>Edition:</strong> <span itemprop="bookEdition">{data.edition}</span>
     </p>
   {/if}
 
-  {#if data.references}
+  {#if data?.references}
     <p>
       <strong>References:</strong> <span itemprop="citation">{data.references}</span>
     </p>
   {/if}
 
-  {#if data.DOI}
+  {#if data?.DOI}
   <p>
     <strong>DOI:</strong> <a href={doiForceLong(data.DOI)} target="_blank" rel="noopener noreferrer" itemprop="url">{doiForceShort(data.DOI)}</a>
   </p>
   {/if}
-  {#if data.URL && urlToDOIShort(data.URL) != data.DOI}
+  {#if data?.URL && urlToDOIShort(data.URL) != data.DOI}
     <p>
       <strong>URL:</strong> <a href={data.URL} target="_blank" rel="noopener noreferrer" itemprop="url">{truncate_url(data.URL)}</a>
     </p>
   {/if}
-  {#if data.status}
+  {#if data?.status}
     <p>
       <strong>Status:</strong>{data.status}
     </p>
   {/if}
 
-  {#if data.custom}
+  {#if data?.custom}
       <strong>Additional Info:</strong>
         <!-- Buttons, including select custom fields-->
         {#if data.custom}
@@ -238,7 +250,7 @@ TODO allow customizing the itemtype
 
   <!-- Buttons, including select custom fields-->
   <div class="flex flex-row">
-    {#if data.custom}
+    {#if data?.custom}
         {#each data.custom as field}
           {#if customFieldsAsButtons.includes(field.key)}
             <button class='pub-button' itemprop="additionalProperty" itemscope itemtype="http://schema.org/PropertyValue">
@@ -250,7 +262,7 @@ TODO allow customizing the itemtype
     {/if}
 
     <!-- Abstract -->
-    {#if data.abstract}
+    {#if data?.abstract}
       <button on:click={() => showAbstract = !showAbstract}
         class='pub-button'>
         ↓ Abstract
@@ -262,7 +274,7 @@ TODO allow customizing the itemtype
     {#if copied}<p class='text-xs italic inline-block text-slate-500 m-1 p-1'>Copied</p>{/if}
 </div>
 
-  {#if data.abstract && showAbstract}
+  {#if data?.abstract && showAbstract}
     <div class="text-xs rounded-lg dark:bg-slate-600 bg-slate-300 p-2 mt-1">
       {data.abstract}
     </div>

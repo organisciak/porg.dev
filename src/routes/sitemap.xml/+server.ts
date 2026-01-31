8ea4650b67b9682d6d2462d82b6c5dfb21224b99
Peter Organisciak <organisciak@gmail.com>
2026-01-30 20:03:40 -0700

    Make SNES homepage the root route

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -4,9 +4,8 @@ export async function GET() {
   return new Response(
     `<?xml version="1.0" encoding="UTF-8" ?>
     <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
-    <url><loc>https://porg.dev</loc></url>
+<url><loc>https://porg.dev</loc></url>
 <url><loc>https://porg.dev/1/</loc></url>
-<url><loc>https://porg.dev/2/</loc></url>
 <url><loc>https://porg.dev/colors/</loc></url>
 <url><loc>https://porg.dev/cv/</loc></url>
 <url><loc>https://porg.dev/huehunter/</loc></url>
